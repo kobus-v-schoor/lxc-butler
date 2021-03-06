5bbff452dbb34e42ff4feba54ea75ecb915d0f65
Kobus van Schoor <v.schoor.kobus@gmail.com>
2021-03-06 18:30:07 +0200

    added workflow to publish pypi package

--- lxcb/info.py
+++ lxcb/info.py
@@ -2,6 +2,14 @@ from os import readlink
 from os.path import expanduser, isfile, relpath
 from getpass import getuser
 
+# package info
+__version__ = '0.0.1'
+__author__ = 'Kobus van Schoor'
+__author_email__ = 'v.schoor.kobus@gmail.com'
+__url__ = 'https://github.com/kobus-v-schoor/lxc-butler'
+__license__ = 'MIT License'
+
+# user info
 username = getuser()
 home = expanduser('~')
 timezone = relpath(readlink('/etc/localtime'), '/usr/share/zoneinfo')
